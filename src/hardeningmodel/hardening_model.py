513b23ace9639945f63a46fd7434d46425f979b1
akoch23 <akoch23@bu.edu>
2025-02-18 11:16:44 -0500

    Update hardening_model.py

--- src/hardeningmodel/hardening_model.py
+++ src/hardeningmodel/hardening_model.py
@@ -30,15 +30,3 @@ class Elastoplastic:
 
   self.sigma_n = sigma_new
   return sigma_new, self.epsilon_p_n
-
-if __name__ == "__main__":
-  E = 210e9
-  H = 1000
-  Y_0 = 250e6
-
-  material = Elastoplastic(E, H, Y_0)
-  delta_epsilon = np.array([0.001, 0, 0])
-  sigma_new, epsilon_p = material.update_stress(delta_epsilon)
-
-  print("Updated Stress: " sigma_new)
-  print("Updated Stress: ", epsilon_p)
