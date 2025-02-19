60fe8da5f8d2462a07ab9e6829faebba11627a83
akoch23 <akoch23@bu.edu>
2025-02-19 12:27:00 -0500

    Update test_hardening_solver.py

--- tests/test_hardening_solver.py
+++ tests/test_hardening_solver.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from hardening_model import Elastoplastic
 
+"""
 material = Elastoplastic (E, H, Y_0)
 strain_incr = np.linspace(0, 0.05, 100)
 stress_values = []
@@ -11,7 +12,23 @@ for delta_epsilon in strain_increment:
   sigma_new, epsilon_p_new = material.check_state(delta_epsilon)
   stress_values.append(np.linalg.norm(sigma_new))
   plastic_strain_values.append(epsilon_p_new)
+"""
 
+steel = Elastoplastic(210, 2.10, 0.250)
+strain_incr = np.linspace(0, 0.05, 100)
+stress_values = []
+plastic_strain_values = []
+
+for delta_epsilon in strain_increment:
+  sigma_new, epsilon_p_new = steel.check_state(delta_epsilon)
+  stress_values.append(np.linalg.norm(sigma_new))
+  plastic_strain_values.append(epsilon_p_new)
+
+def test_isotropic_steel():
+  test_steel = steel
+  assert True
+
+"""
 plt.plot(strain_increments, stress_values, label="Stress")
 plt.xlabel("Strain")
 plt.ylabel("Stress")
@@ -26,3 +43,4 @@ plt.ylabel("Plastic Strain")
 plt.title("Plastic Strain vs Strain")
 plt.grid(True)
 plt.legend()
+"""
