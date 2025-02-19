fade53e98920d7f5816a1cbe84869ea0a8c122c6
akoch23 <akoch23@bu.edu>
2025-02-18 22:31:36 -0500

    Update test_hardening_solver.py

--- tests/test_hardening_solver.py
+++ tests/test_hardening_solver.py
@@ -1,11 +1,28 @@
-if __name__ == "__main__":
-  E = 210e9
-  H = 1000
-  Y_0 = 250e6
+import numpy as np
+import matplotlib.pyplot as plt
+from hardening_model import Elastoplastic
 
-  material = Elastoplastic(E, H, Y_0)
-  delta_epsilon = np.array([0.001, 0, 0])
-  sigma_new, epsilon_p = material.update_stress(delta_epsilon)
+material = Elastoplastic (E, H, Y_0)
+strain_incr = np.linspace(0. 0.05, 100)
+stress_values = []
+plastic_strain_values = []
 
-  print("Updated Stress: " sigma_new)
-  print("Updated Stress: ", epsilon_p)
+for delta_epsilon in strain_increment:
+  sigma_new, epsilon_p_new = material.check_state(delta_epsilon)
+  stress_values.append(np.linalg.norm(sigma_new))
+  plastic_strain_values.append(epsilon_p_new)
+
+plt.plot(strain_increments, stress_values, label="Stress")
+plt.xlabel("Strain")
+plt.ylabel("Stress")
+plt.title("Stress vs. Strain Curve")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+plt.plot(strain_increments, plastic_strain_values, label="Plastic Strain", color='r')
+plt.xlabel("Strain")
+plt.ylabel("Plastic Strain")
+plt.title("Plastic Strain vs Strain")
+plt.grid(True)
+plt.legend()
