b34953608a15796cce6717a1b81ec73ebb7d59b6
akoch23 <akoch23@bu.edu>
2025-02-19 00:32:07 -0500

    Update hardening_model.py

--- src/hardeningmodel/hardening_model.py
+++ src/hardeningmodel/hardening_model.py
@@ -16,18 +16,19 @@ try:
     model_kin = True
     model_iso = True
   else:
-    raise error
+    raise ValueError ("Invalid Hardening Type.")
 '''
 
 @dataclass
 class Elastoplastic:
-  def __init__(self, E: float, H: float, Y_0: float):
+  def __init__(self, E: float, H: float, Y_0: float, hardening_type: str):
     self.E = E
     self.H = H
     self.Y_0 = Y_0
     self.sigma_n = 0
     self.epsilon_p_n = 0
     self.alpha_n = 0
+    self.hardening_type = hardening_type
 
   def compute_yield_stress(self):
     return self.Y_0 + self.H * self.epsilon_p_n
@@ -35,23 +36,23 @@ class Elastoplastic:
   def elastic_predictor(self, delta_epsilon):
     return self.E * delta_epsilon
 
-  def check_state(self, delta_epsilon):
+  def check_state(self, delta_epsilon, hardening_type):
     delta_sigma_trial = self.elastic_predictor(delta_epsilon)
     sigma_trial = self.sigma_n + delta_sigma_trial
 
-    if hardening_type == "Isotropic" or hardening_type = "Both":
+    if self.hardening_type == "Isotropic" or self.hardening_type == "Both":
       Y_trial = self.compute_yield_stress()
     else:
       Y_trial = self.Y_0
 
-    if hardening_type == "Kinematic" or hardening_type == "Both":
+    if self.hardening_type == "Kinematic" or self.hardening_type == "Both":
             alpha_trial = self.alpha_n
         else:
             alpha_trial = 0
           
     nu_trial = sigma_trial - alpha_trial 
-    phi_trial = np.linalg.norm(sigma_trial) - Y_n
-
+    phi_trial = np.linalg.norm(nu_trial) - Y_0
+    
     if phi_trial <= 0:
       sigma_new = sigma_trial
       alpha_new = self.alpha_n
@@ -60,15 +61,20 @@ class Elastoplastic:
     else:
       delta_epsilon_p = phi_trial / (self.E + self.H)
 
-      if hardening_type == "Isotropic" or hardening_type == "Both":
+      if self.hardening_type == "Isotropic" or self.hardening_type == "Both":
         sigma_new = sigma_trial - np.sign(np.linalg.norm(sigma_trial)) * self.E * delta_epsilon_p
+      else:
+        sigma_new = sigma_trial - np.sign(nu_trial) * self.E * delta_epsilon_p
+
+      if self.hardening_type == "Kinematic" or self.hardening_type == "Both":
+        alpha_new = self.alpha_n + np.sign(nu_trial) * self.H * delta_epsilon_p
       else:
         alpha_new = self.alpha_n
 
-      if hardening_type == "Isotropic" or hardening_type == "Both":
+      if self.hardening_type == "Isotropic" or self.hardening_type == "Both":
         Y_new = self.Y_0 + self.H * self.epsilon_p_n
       else:
-        Y_new = self.Y_n
+        Y_new = self.Y_0
 
       epsilon_p_new = self.epsilon_p_n + delta_epsilon_p
 
