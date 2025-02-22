d568891dcb7bbca3a213900771e3c4459e2752c4
akoch23 <akoch23@bu.edu>
2025-02-21 22:19:38 -0500

    Update hardening_model.py

--- src/hardeningmodel/hardening_model.py
+++ src/hardeningmodel/hardening_model.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from dataclasses import dataclass
 
 '''
 hardening_type = input("Please enter which type of hardening model you would like to use for your chosen material: ")
@@ -19,81 +17,86 @@ try:
     raise ValueError ("Invalid Hardening Type.")
 '''
 
-@dataclass
 class Elastoplastic:
-    def __init__(self, E: float, H: float, Y_0: float, hardening_type: str):
-        self.E = E  # Elastic modulus
-        self.H = H  # Hardening modulus
-        self.Y_0 = Y_0  # Initial yield stress
-        self.sigma_n = 0  # Initial stress state
-        self.epsilon_p_n = 0  # Initial plastic strain
-        self.alpha_n = 0  # Kinematic hardening state variable
-        self.hardening_type = hardening_type  # Type of hardening (Isotropic or Kinematic)
-        
-    def elastic_predictor(self, delta_epsilon):
-        # Elastic stress prediction using Hooke's law: sigma = E * epsilon
-        return self.E * delta_epsilon
+  def __init__(self, E: float, H: float, Y_0: float, hardening_type: str):
+      self.E = E  # Elastic modulus
+      self.H = H  # Hardening modulus
+      self.Y_0 = Y_0  # Initial yield stress
+      self.sigma_n = 0  # Initial stress state
+      self.epsilon_p_n = 0  # Initial plastic strain
+      self.alpha_n = 0  # Kinematic hardening state variable
     
-    def check_state(self, delta_epsilon):
-        # Compute trial stress
-        delta_sigma_trial = self.elastic_predictor(delta_epsilon)
-        sigma_trial = self.sigma_n + delta_sigma_trial
+'''
+  def elastic_predictor(self, delta_epsilon):
+      # Elastic stress prediction using Hooke's law: sigma = E * epsilon
+      return self.E * delta_epsilon
+'''
+
+  def yield_stress(self):
+        Y_n = self.Y_0 + self.H * self.epsilon_p_n
+        return Y_n
+
+   def hardening_model(self, strain_change):
+    stress_list = []
+    strain_list = []
+    for i, epsilon_n in enumerate(strain_change):
+        if i == 0:
+            stress_list.append(self.sigma_n)
+            continue
+        delta_epsilon_n = epsilon_n - strain_change[step-1]
+        if np.isclose(delta_epsilon_n, 0):
+            stress_list.append(self.sigma_n)
+            continue
+        self.update_step(delta_epsilon_n)
+        stress_list.append(self.sigma_n)
+        strain_list.append(self.epsilon_p_n)
+    return stress_list
+
+class Isotropic(Elastoplastic):
+  def phi_trial(self, sigma_trial, Y_n):
+      phi_trial = np.abs(sigma_trial) - Y_n
+      return phi_trial
+
+  def check_phi_trial_iso(self, phi_trial, sigma_trial):
+      if phi_trial <= 0: # material deformation is still elastic
+          self.sigma_n = sigma_trial
+          self.epsilon_p_n = self.epsilon_p_n
+      else: # material is yielding/deformation is plastic, return mapping
+          delta_epsilon = phi_trial / (self.E + self.H)
+          self.sigma_n = sigma_trial - np.sign(sigma_trial) * self.E * delta_epsilon
+          self.epsilon_p_n = self.epsilon_p_n + delta_epsilon
+          
+  def step_forward_iso(self, delta_epsilon):
+      Y_n = self.yield_stress()
+      delta_sigma_trial = self.E * delta_epsilon
+      sigma_trial = self.sigma_n + delta_sigma_trial
+      phi_trial = self.phi_trial(sigma_trial,Y_n)
+      self.check_phi_trial(phi_trial,sigma_trial)
+   # How to return mapping?
+
+class Kinematic(Elastoplastic):
+  def __init__(self, E, H, Y_0, alpha_n):
+      super().__init__(E, H, Y_0, alpha_n)
+    
+  def check_phi_trial_kin(self,phi_trial, sigma_trial, alpha_trial, eta_trial):
+      if phi_trial <= 0: # material deformation is still elastic
+          self.sigma_n = sigma_trial
+          self.alpha_n = alpha_trial
+          self.epsilon_p_n = self.epsilon_p_n
+      else: # material is yielding/deformation is plastic, return mapping
+          delta_epsilon = phi_trial / (self.E + self.H)
+          self.sigma_n = sigma_trial - np.sign(eta_trial) * self.E * delta_epsilon
+          self.alpha_n = self.alpha_n + np.sign(eta_trial) * self.H * delta_epsilon
+          self.epsilon_p_n = self.epsilon_p_n + delta_epsilon
+
+  def step_foward_kin(self, delta_epsilon):
+        # Elastic predictor step with sigma trial
+        sigma_trial = self.sigma_n + self.E * delta_epsilon
+        alpha_trial = self.alpha_n
+        eta_trial = sigma_trial - alpha_trial
+        phi_trial = np.abs(eta_trial) - self.Y_0
+
+        # Check if the material is experiencing elastic or plastic deformation and update values accordingly
+        self.check_phi_trial(phi_trial,sigma_trial,alpha_trial,eta_trial)
         
-        # Check hardening type and compute corresponding trial yield condition
-        if self.hardening_type == "Isotropic":
-            Y_n = self.Y_0 + self.H * self.epsilon_p_n  # Yield stress with isotropic hardening
-            phi_trial_iso = np.linalg.norm(sigma_trial) - Y_n  # Yield condition for isotropic
-            phi_trial = phi_trial_iso
-
-        elif self.hardening_type == "Kinematic":
-            alpha_trial = self.alpha_n  # Kinematic hardening state
-            nu_trial = sigma_trial - alpha_trial  # Trial stress for kinematic model
-            Y_n = self.Y_0  # Constant yield stress in kinematic hardening
-            phi_trial_kin = np.linalg.norm(nu_trial) - Y_n  # Yield condition for kinematic
-            phi_trial = phi_trial_kin
-
-        elif self.hardening_type == "Both":
-            # Isotropic + Kinematic combined
-            Y_n = self.Y_0 + self.H * self.epsilon_p_n  # Isotropic hardening contribution
-            alpha_trial = self.alpha_n
-            nu_trial = sigma_trial - alpha_trial  # Combined trial stress for both models
-            phi_trial_iso = np.linalg.norm(sigma_trial) - Y_n
-            phi_trial_kin = np.linalg.norm(nu_trial) - Y_n
-            phi_trial = max(phi_trial_iso, phi_trial_kin)  # Choose the max phi from both models
-        
-        # Check if material is elastic or plastic
-        if phi_trial <= 0:  # Elastic regime
-            sigma_new = sigma_trial
-            epsilon_p_new = self.epsilon_p_n
-            alpha_new = self.alpha_n
-            Y_new = Y_n  # Yield stress (can be isotropic or constant)
-        else:  # Plastic regime
-            delta_epsilon_p = phi_trial / (self.E + self.H)
-            
-            if self.hardening_type == "Isotropic":
-                sigma_new = sigma_trial - np.sign(np.linalg.norm(sigma_trial)) * self.E * delta_epsilon_p
-            elif self.hardening_type == "Kinematic":
-                sigma_new = sigma_trial - np.sign(nu_trial) * self.E * delta_epsilon_p
-                alpha_new = self.alpha_n + np.sign(nu_trial) * self.H * delta_epsilon_p
-            elif self.hardening_type == "Both":
-                # In both models, consider contributions from both isotropic and kinematic
-                sigma_new = sigma_trial - np.sign(nu_trial) * self.E * delta_epsilon_p
-                alpha_new = self.alpha_n + np.sign(nu_trial) * self.H * delta_epsilon_p
-                # For combined hardening, we can also adjust yield stress based on both models
-
-            epsilon_p_new = self.epsilon_p_n + delta_epsilon_p
-            Y_new = Y_n
-
-        # Update internal state
-        self.sigma_n = sigma_new
-        self.alpha_n = alpha_new
-        self.Y_n = Y_new
-        self.epsilon_p_n = epsilon_p_new
-
-        # Return updated values depending on hardening model
-        if self.hardening_type == "Isotropic":
-            return sigma_new, epsilon_p_new
-        elif self.hardening_type == "Kinematic":
-            return sigma_new, alpha_new, epsilon_p_new
-        elif self.hardening_type == "Both":
-            return sigma_new, alpha_new, epsilon_p_new  # Could return a combination of both states
+      
